fd2ea2c45f1d64ee7308f4a79115e2a086853ea0
AlinaChorna7 <159713721+AlinaChorna7@users.noreply.github.com>
2025-02-25 21:20:56 +0100

    end signup start logout

--- src/components/SignUp/signUp.jsx
+++ src/components/SignUp/signUp.jsx
@@ -1,81 +1,113 @@
-import { useEffect } from "react";
+
+import { useState } from "react";
 import "./signUp.css";
+import { BaseURL } from "../../services/ApiIntegration";
+import { useNavigate } from "react-router-dom";
+
 
 export const SignUp = () => {
-  const [isModalOpen, setIsModalOpen] = useState(false);
 
-  const openModal = () => setIsModalOpen(true);
-  const closeModal = () => setIsModalOpen(false);
+const [email, setEmail] = useState("");
+const [password, setPassword] = useState("");
+const navigate = useNavigate();
 
-  useEffect(() => {
-    if (isModalOpen) {
-      document.body.classList.add("modal-open");
-    } else {
-      document.body.classList.remove("modal-open");
-    }
-  }, [isModalOpen]);
+const handleSubmit = async (event)=>{
+  event.preventDefault();
 
-  if (!isModalOpen) return null;
+if(!email || !password){
+  return alert("Please enter your email and password");
+}
 
-  return (
-    <div className="backdrop" onClick={closeModal}>
-      <div className="modal" onClick={(e) => e.stopPropagation()}>
-        <div className="header-modal">
-          <h3 className="modal-title">Sign Up</h3>
-          <button onClick={closeModal} className="close-modal-btn">
-            <svg width="28px" height="28px" className="modal-close-icon">
-              <use href="/images/icons.svg#icon-x-mark" />
-            </svg>
-          </button>
-        </div>
-        <div className="modal-form-cnt">
-          <form className="modal-form">
-            <input
-              type="email"
-              name="user-email"
-              className="modal-input"
-              id="user-email"
-              placeholder="Please enter your email"
-            />
-            <input type="password"
-             name="user-passsword"
+
+try {
+const response = await fetch(`${BaseURL}/register`,{
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+  },
+  body: JSON.stringify({ email, password }),
+});
+
+const data = await response.json();
+
+if(response.ok){
+  alert("Registration begin successfully!");
+  navigate("/login");
+  return 
+}else{
+  alert(data.message || "Registration error");
+}
+  }
+ catch (error) {
+  console.error("Помилка:", error);
+  alert("Сталася помилка при реєстрації");
+};
+};
+return (
+  <div className="backdrop">
+    <div className="modal" onClick={(e) => e.stopPropagation()}>
+      <div className="header-modal">
+        <h3 className="modal-title">Sign Up</h3>
+    
+      </div>
+      <div className="modal-form-cnt">
+        <form className="modal-form" onSubmit={handleSubmit}>
+          <input
+            type="email"
+            name="user-email"
             className="modal-input"
-             id="user-password" 
-             placeholder="Please enter your password"/>
-            <div className="form-btns">
-              <button className="modal-link">Sign up</button>
-              <button className="google-modal">
-                <svg width="15px" height="15px" className="google-login-icon">
-                  <use href="./images/icons.svg#icon-google" />
-                </svg>
-                <p className="google-enter">Or sign up with Google</p>
-              </button>
-            </div>
-          </form>
+            id="user-email"
+            placeholder="Please enter your email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+          <input
+            type="password"
+            name="user-password"
+            className="modal-input"
+            id="user-password"
+            placeholder="Please enter your password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+          <div className="form-btns">
+            <button type="submit" className="modal-link">
+              Sign up
+            </button>
+            <button type="button" className="google-modal">
+              <svg width="15px" height="15px" className="google-login-icon">
+                <use href="./images/icons.svg#icon-google" />
+              </svg>
+              <p className="google-enter">Or sign up with Google</p>
+            </button>
+          </div>
+        </form>
+      </div>
+      <div className="wallet-conection-div">
+        <div className="walet-modal-title">
+          <svg width="15px" height="15px" className="wallet-modal-icon">
+            <use href="/images/icons.svg#icon-wallet" />
+          </svg>
+          <p className="conect-wallet">OR CONNECT WALLET</p>
         </div>
-        <div className="wallet-conection-div">
-          <div className="walet-modal-title">
-            <svg width="15px" height="15px" className="wallet-modal-icon">
-              <use href="/images/icons.svg#icon-wallet" />
+        <div className="wallets-selection">
+          <button className="metamask-btn">
+            <svg width="15px" height="15px" className="metamask-icon">
+              <use href="/images/icons.svg#metamask" />
             </svg>
-            <p className="conect-wallet">OR CONNECT WALLET</p>
-          </div>
-          <div className="wallets-selection">
-            <button className="metamask-btn">
-              <svg width="15px" height="15px" className="metamask-icon">
-                <use href="/images/icons.svg#metamask" />
-              </svg>
-              <span className="metamask-p">MetaMask</span>
-            </button>
-            <button className="other-wallets-btn">
-              <svg width="15px" height="15px" className="other-wallets-icon">
-                <use href="/images/icons.svg#wallets-icon" />
-              </svg>
-              <span>Other wallets</span>
-            </button>
-          </div>
+            <span className="metamask-p">MetaMask</span>
+          </button>
+          <button className="other-wallets-btn">
+            <svg width="15px" height="15px" className="other-wallets-icon">
+              <use href="/images/icons.svg#wallets-icon" />
+            </svg>
+            <span>Other wallets</span>
+          </button>
         </div>
       </div>
     </div>
-  );
+  </div>
+);
 };
